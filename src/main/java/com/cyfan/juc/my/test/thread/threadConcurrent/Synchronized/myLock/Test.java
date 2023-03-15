d77ee5e6aaad908e9a7e9a3670b697629458bbd0
cyfan <1971380780@qq.com>
2023-03-16 03:16:32 +0800

    aqs实现自定义锁，jol查看对象头bit位证明无锁、偏向锁、轻量级锁、重量级锁

---
+++ src/main/java/com/cyfan/juc/my/test/thread/threadConcurrent/Synchronized/myLock/Test.java
@@ -0,0 +1,31 @@
+package com.cyfan.juc.my.test.thread.threadConcurrent.Synchronized.myLock;
+
+public class Test {
+
+    static int COUNT = 500;
+    final static MyLock myLock =  new MyLock();
+    public static void main(String[] args) {
+        Test test = new Test();
+        for (int i = 0; i < 5; i++) {
+            new Thread(()->{
+                test.test();
+            }).start();
+        }
+    }
+
+    private void test() {
+
+        for (;;){
+
+            try {
+                myLock.lock();
+                if (COUNT <= 0){
+                    break;
+                }
+                System.out.println("current Thread :" + Thread.currentThread().getName() + ", COUNT = "+ (--COUNT));
+            } finally {
+                myLock.unlock();
+            }
+        }
+    }
+}
